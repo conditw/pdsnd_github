b110e6f6d6e8a3af91d892544cfe28e973feeed5
Wendy-Condit <conditw@battelle.org>
2020-05-02 13:05:13 -0400

    Change Hr Calc

--- BikeshareRScript.R
+++ BikeshareRScript.R
@@ -3,11 +3,11 @@ summary(bk)
 install.packages('ggplot2')
 library(ggplot2)
 names(bK)
-qplot(x = Gender, y = (2020-Birth.Year), data = bk, 
+qplot(x = Gender, y = (2020-Birth.Year), data = bk,
       geom = 'boxplot',ylim=c(20, 80), main = 'Biker Share Users Age', xlab = 'Gender', ylab = 'Age, Years')
-qplot(x = Gender, y = (Trip.Duration/60), data = bk, 
-      geom = 'boxplot',ylim=c(0,60), main = 'Trip Duration', xlab = 'Gender', ylab = 'Trip Duration, Minutes')
-ggplot(aes(x = (2020-Birth.Year), y = Trip.Duration/60), data = bk)+ xlim(20,80) + ylim(0, 60)+ geom_point(alpha = 0.05, position = position_jitter(h = 0),color='blue')+ 
+qplot(x = Gender, y = (Trip.Duration), data = bk, 
+      geom = 'boxplot',ylim=c(0,60), main = 'Trip Duration, min', xlab = 'Gender', ylab = 'Trip Duration, Minutes')
+ggplot(aes(x = (2020-Birth.Year), y = Trip.Duration/60), data = bk)+ xlim(20,80) + ylim(0, 60)+ geom_point(alpha = 0.05, position = position_jitter(h = 0),color='blue')+
   ggtitle('Duration of Trips by Age') +
   labs(x = "Age, Years", y = "Trip Duration, Minutes")
 names(bk)
